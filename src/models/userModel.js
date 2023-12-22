8ab8076c626f6d2bd396e79d0b6d70e8a2d1e242
fatimememmedli <bd7u8hqcl@code.edu.az>
2023-12-22 18:01:36 +0400

    asd

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,12 +2,21 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
   {
-    name: String,
+    username: String,
+    surname: String,
+    following: Array,
+    follower: Array,
+    blockList: Array,
+    stories: Array,
+    notifications: Array,
+    posts: Array,
     id: String,
+    isPublic: Boolean,
+    bio: Object,
+    email: String,
   },
   { collection: "Users", timestamps: true }
 );
 
 const User = mongoose.model("Users", userSchema);
-
 module.exports = User;
